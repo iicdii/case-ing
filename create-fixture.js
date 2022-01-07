c46b38e54a5af906cc78354aa4aafeaf16a98342
harimkims <harimkims@gmail.com>
2022-01-07 20:36:24 +0900

    Create fixture folder if not exists

--- create-fixture.js
+++ create-fixture.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 const axios = require('axios');
 const fs = require('fs');
 
+const FIXTURE_FOLDER_PATH = './cypress/fixtures';
+
+if (!fs.existsSync(FIXTURE_FOLDER_PATH)){
+  fs.mkdirSync(FIXTURE_FOLDER_PATH);
+}
+
 axios
   .get(`${process.env.CYPRESS_LAMBDA_API_URL}/cases`)
   .then(res => {
-    fs.writeFileSync('./cypress/fixtures/cases.json', JSON.stringify(res.data.data.slice(1)));
+    fs.writeFileSync(`${FIXTURE_FOLDER_PATH}/cases.json`, JSON.stringify(res.data.data.slice(1)));
   })
   .catch(e => console.error(e))
