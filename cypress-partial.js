de4d195bf107dc56032b6d2227c7e69de68f4ec1
harimkims <harimkims@gmail.com>
2022-10-09 21:35:42 +0900

    Rename e2e spec path

--- cypress-partial.js
+++ cypress-partial.js
@@ -1,6 +1,6 @@
 /**
  * Primitive load-balancer to split Cypress specs across multiple runners. This script assumes that
- * all your specs are in the folder '<project root>/cypress/integration'. It uses the number of
+ * all your specs are in the folder '<project root>/cypress/e2e'. It uses the number of
  * tests per spec file as sole criteria to split specs between runners.
  *
  * This script accepts two arguments: the total number of runners and the index (starting from 0) of
@@ -8,7 +8,7 @@
  *    $ node cypress-partial.js 5 2
  * This command asks for specs to give to the third runner of five runners.
  * The output of the script is a coma-separated list of specs that can be given to Cypress. Example:
- *    cypress/integration/some.spec.ts,cypress/integration/another.spec.ts
+ *    cypress/e2e/some.spec.ts,cypress/e2e/another.spec.ts
  *
  * Let's assume you have five runners (virtual machines) to run specs in parallel on your CI, and
  * each runner knows its number (from 0 to 4). So the third runner must execute its spec as
@@ -31,7 +31,7 @@
 
 const fs = require('fs/promises');
 
-const specDir = 'cypress/integration';
+const specDir = 'cypress/e2e';
 const testPattern = /(^|\s)(it|test)\(/g;
 
 const [ totalRunners, thisRunner ] = process.argv.splice(2);
